8bdc01288b0a158f1ba3113507220816a5be071e
carlitos321 <ca.cabrera321@gmail.com>
2018-01-17 20:26:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks() {
+  
+}
+
+function distanceFromHqInFeet() {
+  
+}
+
+
+function calculateVertical() {
+  
+}
+
+function distanceTravelledInFeet() {
+  
+}
+
+function calculatesFarePrice() {
+  
+}
